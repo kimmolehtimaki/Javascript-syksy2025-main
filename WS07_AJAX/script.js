3db39af935f3b37b9523715e6b2f8052c7af07b6
kimmolehtimaki <125656203+kimmolehtimaki@users.noreply.github.com>
2025-10-30 23:29:12 +0200

    second commit with added solutions

--- WS07_AJAX/script.js
+++ WS07_AJAX/script.js
@@ -97,3 +97,69 @@ function parseRawData() {
         }
     }
 };
+
+// Tehtävä 3 ratkaisut
+
+// luodaan funktio ja AJAX-olio datan hakemiseen
+function fetchRawWeather() {
+    var xmlhttp = new XMLHttpRequest();
+    //lisätään API-kutsuun lang=fi , jotta saadaan suomenkieliset kaupunkien nimet
+    xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&lang=fi&units=metric&mode=JSON&APPID=2d45a57e705bccf0e859e32fcfc0a798", true);
+    xmlhttp.send();
+
+    //luodaan vastauksen käsittelijä
+    xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                let jsonwe = xmlhttp.responseText;
+                //lisätään sisältö div:iin
+                document.getElementById("weatherdata").innerHTML = jsonwe;
+            
+                
+        }
+
+    }
+};
+
+//Kaupungin valinta puotusvalikon kautta
+
+function selectCity() {
+    //muuttuja kaupungille
+    let city = document.getElementById("city").value;
+            
+    var xmlhttp = new XMLHttpRequest();
+    //määritetään dynaamisesti muuttuja URL:lle, jotta saadaan API-kutsu muuttumaan käyttäjän valinnan mukaisesti
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fi&units=metric&mode=JSON&APPID=2d45a57e705bccf0e859e32fcfc0a798`;
+
+    xmlhttp.open("GET", url, true);
+    //lähetetään kutsu
+    xmlhttp.send();
+
+    //luodaan vastauksen käsittelijä
+    xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                const jsonwe = JSON.parse(xmlhttp.responseText);
+
+                const table = `
+                                <table border="1">
+                                <tr>
+                                    <th>Kaupunki</th>
+                                    <th>Lämpötila</th>
+                                    <th>Säätila</th>
+                                    <th>Ilman kosteus</th>
+                                    <th>Ikoni</th>
+                                </tr>
+                                <tr>
+                                    <td>${jsonwe.name}</td>
+                                    <td>${jsonwe.main.temp}</td>
+                                    <td>${jsonwe.weather[0].description}</td>
+                                    <td>${jsonwe.main.humidity}%</td>
+                                    <td><img src="https://openweathermap.org/img/w/${jsonwe.weather[0].icon}.png></td>
+                                    
+                                </tr>
+                                </table>`;
+                
+                //lisätään sisältö div:iin
+                document.getElementById("weatherdata").innerHTML = table;
+            } //if päättyy
+        }
+    };
